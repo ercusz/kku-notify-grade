86c43a362354b8e73c061a168980cd5b3336f148
ercusz <eurokungz.xkz@gmail.com>
2022-06-25 23:24:31 +0700

    fix: :bug: prevent request blocking in 'getCourseData' function

--- grade_status.py
+++ grade_status.py
@@ -1,9 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 def getCourseData(acadyear: int, semester: int, course_code: str, sec: int):
+    
     url = f'https://reg-mirror.kku.ac.th/registrar/class_info_status.asp?acadyear={acadyear}&semester={semester}&coursecode={course_code}'
-    req = requests.get(url)
+    
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    
+    req = session.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
 
     for tr in soup.find_all('tr', class_='NormalDetail'):
